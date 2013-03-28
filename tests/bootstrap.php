e811e1eb9819b3184b7017728247669f1b3938b4
Michael van Engelshoven <michael@van-engelshoven.de>
2013-03-28 16:23:54 +0100

    Added config file for travis ci
    Need to fix paths in test boostrap file

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -11,12 +11,12 @@
 error_reporting(-1);
 date_default_timezone_set('Europe/Berlin');
 
-if (file_exists('../vendor/autoload.php'))
+if (file_exists(__DIR__ . '/../vendor/autoload.php'))
 {
-    require_once '../vendor/autoload.php';
+    require_once __DIR__ . '/../vendor/autoload.php';
 }
 else
 {
-    require_once '../../../autoload.php';
+    require_once __DIR__ . '/../../../autoload.php';
 }
 
